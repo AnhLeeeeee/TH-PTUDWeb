847788a6dc353a15ebc96d98544978138f0f61fe
AnhLeeeeee <levananh13032k4@gmail.com>
2024-11-11 17:36:52 +0700

    push src

---
+++ Models/TbOrderDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Harmic2.Models;
+
+public partial class TbOrderDetail
+{
+    public int OrderDetailId { get; set; }
+
+    public int? OrderId { get; set; }
+
+    public int? ProductId { get; set; }
+
+    public decimal? Price { get; set; }
+
+    public int? Quantity { get; set; }
+
+    public virtual TbProduct? Product { get; set; }
+}
---
+++ Views/Shared/Components/MenuTop/Default.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Harmic2.Views.Components.MenuTop
+{
+    public class DefaultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
